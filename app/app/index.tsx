05fb758e32428ff6830a8ba1e634e949fb3ce6a9
Kyrylo Muzychka <kyryll2016@gmail.com>
2024-11-14 17:50:50 +0200

    added animation

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,26 +1,7 @@
 import SplashScreen from "../src/screens/Splash";
-import { SafeAreaView, StatusBar, Text, View } from "react-native";
-
 
 export default function Index() {
   return (
-    <SafeAreaView style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'black'
-    }}>
-      <StatusBar barStyle='light-content' />
-      <SplashScreen />
-      <View>
-        <Text style={{
-          color: 'white',
-          textAlign: 'center',
-          fontSize: 48
-        }}>
-          RealTime
-        </Text>
-      </View>
-    </SafeAreaView>
+    <SplashScreen/>
   );
 }
--- app/src/screens/Splash.jsx
+++ app/src/screens/Splash.jsx
@@ -1,7 +1,43 @@
-import { Text } from "react-native"
+import { useEffect } from "react";
+import { Animated, SafeAreaView, StatusBar, View, Text } from "react-native";
+import Title from "../common/Title";
 
 function SplashScreen() {
-    return <Text>Splash</Text>
+  const translateY = new Animated.Value(0);
+  const duration = 800;
+
+  useEffect(() => {
+    Animated.loop(
+        Animated.sequence([
+        Animated.timing(translateY, {
+            toValue: 20,
+            duration: duration,
+            useNativeDriver: true,
+        }),
+        Animated.timing(translateY, {
+            toValue: 0,
+            duration: duration,
+            useNativeDriver: true,
+        }),
+        ])
+    ).start();
+  }, []);
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "black",
+      }}
+    >
+      <StatusBar barStyle="light-content" />
+      <Animated.View style={{ transform: [{ translateY }] }}>
+        <Title text="hello" color="#fff" />
+      </Animated.View>
+    </SafeAreaView>
+  );
 }
 
-export default SplashScreen
+export default SplashScreen;
